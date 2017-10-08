209358e152e055af10282f9136828df7550a535a
Jakir Patel <jakir@voyalab.com>
2017-10-08 21:10:24 +0530

    Changed the message.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World! How are you?');
 });
 
 app.listen(3000, function () {

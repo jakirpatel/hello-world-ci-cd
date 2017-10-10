04dbdfb3d087a431020b37b6ec8394d1bb08c172
Jakir Patel <jakir@voyalab.com>
2017-10-10 12:28:02 +0530

    Modified the file of the server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World! This is DevOps!');
+    res.send('Hello World! This is DevOps! And its awesome!');
 });
 
 app.listen(3000, function () {

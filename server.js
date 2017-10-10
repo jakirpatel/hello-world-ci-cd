f4df32cca405614bb5876a9423f85f888d353e73
Jakir Patel <jakir@voyalab.com>
2017-10-10 12:41:29 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World! This is DevOps! And its awesome!');
+    res.send('Hello World! This is DevOps! And its awesome! And It is!');
 });
 
 app.listen(3000, function () {
